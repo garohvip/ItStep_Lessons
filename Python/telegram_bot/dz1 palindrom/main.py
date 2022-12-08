a316ef4b50c96f26b0b3bb2c77ecd0d744b9160c
garohvip <garohvip@mail.ru>
2022-12-08 23:29:34 +0200

    telegram

--- Python/telegram_bot/dz1 palindrom/main.py
+++ Python/telegram_bot/dz1 palindrom/main.py
@@ -1,44 +1,158 @@
+import json
 import telebot
 import re
 
 config = {
     "name": "",
-    "token": "5887337428:AAFRKUrVND6fjISP5DHOdzTadUG2SSK-z-8"
+    "token": "TOKEN"
 }
 
 agent_ghost = telebot.TeleBot(config["token"])
+
+keyboard_start = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_start.add(telebot.types.KeyboardButton("регистрация".title()),
+                   telebot.types.KeyboardButton("авторизация".title()),
+                   telebot.types.KeyboardButton("остаться гостем".capitalize()))
+
+keyboard_guest = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_guest.add(telebot.types.KeyboardButton("hi"),
+                   telebot.types.KeyboardButton("регистрация".title()),
+                   telebot.types.KeyboardButton("авторизация".title()),)
+
+keyboard_lvl_1 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_lvl_1.add(telebot.types.KeyboardButton("палиндром".title()),
+                   telebot.types.KeyboardButton("парное".title()),
+                   telebot.types.KeyboardButton("lucky number".capitalize()),
+                   telebot.types.KeyboardButton("выйти с аккаунта".capitalize()))
+
+keyboard_lvl_2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_lvl_2.add(telebot.types.KeyboardButton("палиндром".title()),
+                   telebot.types.KeyboardButton("парное".title()),
+                   telebot.types.KeyboardButton("калькулятор".title()),
+                   telebot.types.KeyboardButton("lucky number".capitalize()),
+                   telebot.types.KeyboardButton("статистика".title()),
+                   telebot.types.KeyboardButton("выйти с аккаунта".capitalize()))
+
+
+keyboard_all = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_all.add(telebot.types.KeyboardButton("регистрация".title()),
+                 telebot.types.KeyboardButton("авторизация".title()),
+                 telebot.types.KeyboardButton("палиндром".title()),
+                 telebot.types.KeyboardButton("парное".title()),
+                 telebot.types.KeyboardButton("калькулятор".title()),
+                 telebot.types.KeyboardButton("lucky number".capitalize()),
+                 telebot.types.KeyboardButton("статистика".title()),
+                 telebot.types.KeyboardButton("выйти с аккаунта".capitalize()))
+
+keyboard_statistic = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_statistic.add(telebot.types.KeyboardButton("всю информацию".capitalize()),
+                       telebot.types.KeyboardButton("количество слов".capitalize()),
+                       telebot.types.KeyboardButton("количество символов".capitalize()),
+                       telebot.types.KeyboardButton("количество гласных букв".capitalize()),
+                       telebot.types.KeyboardButton("количество согласных букв".capitalize()),
+                       telebot.types.KeyboardButton("количество гласных и согласных букв".capitalize()),
+                       telebot.types.KeyboardButton("количество цифр".capitalize()),
+                       telebot.types.KeyboardButton("количество спец. символов".capitalize()),
+                       telebot.types.KeyboardButton("назад в меню".capitalize()))
+
+
+@agent_ghost.message_handler(commands=["start", "buttons"])
+def start(message):
+    if message.text == "/start":
+        agent_ghost.send_message(message.chat.id, "Hi, i'm bot. Как насчет чтобы авторизоваться?", reply_markup=keyboard_start)
+    elif message.text == "/buttons":
+        with open("reg.json", "r") as file:
+            all_info = json.load(file)
+        for i in all_info.get('musicbot'):
+            if i.get('user_id') == message.from_user.id:
+                if i.get('lvlrights') == 1:
+                    agent_ghost.send_message(message.chat.id, "Выбери действие", reply_markup=keyboard_lvl_1)
+                elif i.get('lvlrights') == 2:
+                    agent_ghost.send_message(message.chat.id, "Выбери действие", reply_markup=keyboard_lvl_2)
+                elif i.get('lvlrights') == 10:
+                    agent_ghost.send_message(message.chat.id, "Выбери действие", reply_markup=keyboard_all)
 
 
 # принимаем текст и делаем вывод
 @agent_ghost.message_handler(content_types=["text"])
 def get_text(message):
+    global userid
+    global lvlright
+    userid = 0
+    lvlright = 0
+    with open("reg.json", "r") as file:
+        all_users = json.load(file)
+    for i in all_users.get('musicbot'):
+        if i.get('user_id') == message.from_user.id:
+            userid = i.get('user_id')
+            lvlright = i.get('lvlrights')
     if message.text == "hi":
-        agent_ghost.send_message(message.chat.id, "Hello")
+        agent_ghost.send_message(message.chat.id, "hello " + message.from_user.username)
     elif message.text.lower() == "палиндром":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Ну ка давай сюда слово, сейчас я его проверю на палиндром"), palindrom)
+        if userid == message.from_user.id and lvlright >= 1:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Ну ка давай сюда слово, сейчас я его проверю на палиндром"), palindrome)
+        else:
+            agent_ghost.send_message(message.chat.id, "Доступа нет!")
     elif message.text.lower() == "парное":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Кидай цифру"), par)
+        if userid == message.from_user.id and lvlright >= 1:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Кидай цифру"), par)
+        else:
+            agent_ghost.send_message(message.chat.id, "Доступа нет!")
     elif message.text.lower() == "калькулятор":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Пиши пример в одну строку"), calculator)
+        if userid == message.from_user.id and lvlright >= 2:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Пиши пример в одну строку"), calculator)
+        else:
+            agent_ghost.send_message(message.chat.id, "Доступа нет!")
     elif message.text.lower() == "lucky number":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Пиши число, сейчас проверю"), lucky_number)
+        if userid == message.from_user.id and lvlright >= 1:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Пиши число, сейчас проверю"), lucky_number)
+        else:
+            agent_ghost.send_message(message.chat.id, "Доступа нет!")
     elif message.text.lower() == "регистрация":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Введите логин"), registration_login)
+        if userid == message.from_user.id and lvlright >= 1:
+            agent_ghost.send_message(message.chat.id, "Вы уже авторизованы!")
+        else:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Введите логин"), registration_login)
     elif message.text.lower() == "авторизация":
-        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Введите логин"), authorization_login)
+        if userid == message.from_user.id and lvlright >= 1:
+            agent_ghost.send_message(message.chat.id, "Вы уже авторизованы!")
+        else:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Введите логин"), authorization_login)
+    elif message.text.lower() == "статистика":
+        if userid == message.from_user.id and lvlright >= 2:
+            agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Что конкретно хотите вывести", reply_markup=keyboard_statistic), statistic_check_message)
+        else:
+            agent_ghost.send_message(message.chat.id, "Доступа нет!")
+    elif message.text.lower() == "выйти с аккаунта":
+        if userid == message.from_user.id and lvlright >= 1:
+            logout_account(message)
+            agent_ghost.send_message(message.chat.id, "Вы успешно вышли с аккаунта!", reply_markup=keyboard_start)
+        else:
+            agent_ghost.send_message(message.chat.id, "Вы не авторизованы!")
+    elif message.text.lower() == "остаться гостем":
+        agent_ghost.send_message(message.chat.id, "Теперь ты гость.\nФункционал гостя слишком ограничен, по-этому рекомендую зарегистрироваться или же авторизоваться.", reply_markup=keyboard_guest)
 
 
-# функция регистрации с проверкой на наличие логина в БД (текстовом файле)
+# функция выхода из аккаунта (по факту смена user_id на None)
+def logout_account(message):
+    with open("reg.json", "r") as file:
+        all_info = json.load(file)
+    for i in range(len(all_info.get('musicbot'))):
+        if all_info['musicbot'][i]['user_id'] == message.from_user.id:
+            all_info['musicbot'][i]['user_id'] = 0
+    with open("reg.json", "w") as file:
+        return json.dump(all_info, file)
+
+
+# функция регистрации с проверкой на наличие логина в БД (json файле)
 def registration_login(message):
     def check_login(login_var):
-        with open("reg.txt", "r") as file:
-            all_info = file.readlines()
-        all_info = [line.rstrip() for line in all_info]
-        for i in range(0, len(all_info), 4):
-            if login_var == all_info[i]:
+        with open("reg.json", "r") as file:
+            all_info = json.load(file)
+        for i in all_info.get("musicbot"):
+            if login_var == i.get('login'):
                 return False
         return True
-
     global login
     login = message.text
     if check_login(login):
@@ -59,12 +173,15 @@ def registration_password(message):
 # продолжение регистрации, ввод имени
 def registration_name(message):
     name = message.text
-    with open("reg.txt", "a") as file:
-        file.write(f"""{login.lower()}\n{password}\n{name.title()}\n\n""")
-    return agent_ghost.send_message(message.chat.id, "Данные успешно внесены")
+    with open("reg.json", "r") as file:
+        var_for_edit = json.load(file)
+    var_for_edit.get('musicbot').append({"login": login.lower(), "password": password, "name": name.title(), "lvlrights": 1, "user_id": message.from_user.id})
+    with open("reg.json", "w") as file:
+        json.dump(var_for_edit, file)
+    return agent_ghost.send_message(message.chat.id, "Успешная регистрация!", reply_markup=keyboard_lvl_1)
 
 
-# авторизация без проверки на наличие логина в БД (текстовом файле)
+# авторизация без проверки на наличие логина в БД (json файле)
 def authorization_login(message):
     global login_search
     login_search = message.text
@@ -73,15 +190,76 @@ def authorization_login(message):
 
 # продолжение авторизации с проверкой на совпадение логин и пароля
 def authorization_password(message):
-    with open("reg.txt", "r") as file:
-        all_info = file.readlines()
-    all_info = [line.rstrip() for line in all_info]
+    with open("reg.json", "r") as file:
+        all_info = json.load(file)
     password_search = message.text
-    for i in range(0, len(all_info), 4):
-        if login_search == all_info[i]:
-            if password_search == all_info[i+1]:
-                return agent_ghost.send_message(message.chat.id, f"Добро пожаловать, {all_info[i+2]}")
-    return agent_ghost.send_message(message.chat.id, "Логин или пароль не верны!")
+    for i in range(len(all_info.get('musicbot'))):
+        if login_search == all_info.get('musicbot')[i].get("login"):
+            if password_search == all_info.get('musicbot')[i].get("password"):
+                all_info['musicbot'][i]['user_id'] = message.from_user.id
+                with open("reg.json", "w") as file:
+                    json.dump(all_info, file)
+                if all_info.get('musicbot')[i].get('lvlrights') == 1:
+                    return agent_ghost.send_message(message.chat.id, f"Добро пожаловать, {all_info.get('musicbot')[i].get('name')}", reply_markup=keyboard_lvl_1)
+                elif all_info.get('musicbot')[i].get('lvlrights') == 2:
+                    return agent_ghost.send_message(message.chat.id, f"Добро пожаловать, {all_info.get('musicbot')[i].get('name')}", reply_markup=keyboard_lvl_2)
+                elif all_info.get('musicbot')[i].get('lvlrights') == 10:
+                    return agent_ghost.send_message(message.chat.id, f"Добро пожаловать, {all_info.get('musicbot')[i].get('name')}", reply_markup=keyboard_all)
+    return agent_ghost.send_message(message.chat.id, "Логин или пароль введен не верно!")
+
+
+# запрос конкретного действия с текстом
+def statistic_check_message(message):
+    if message.text == "Назад в меню":
+        with open("reg.json", "r") as file:
+            all_info = json.load(file)
+        for i in all_info.get('musicbot'):
+            if i.get('user_id') == message.from_user.id:
+                if i.get('lvlrights') == 1:
+                    agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Ты в меню", reply_markup=keyboard_lvl_1), get_text)
+                elif i.get('lvlrights') == 2:
+                    agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Ты в меню", reply_markup=keyboard_lvl_2), get_text)
+                elif i.get('lvlrights') == 10:
+                    agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Ты в меню", reply_markup=keyboard_all), get_text)
+    else:
+        global message_user
+        message_user = message.text
+        agent_ghost.register_next_step_handler(agent_ghost.send_message(message.chat.id, "Введите текст"), statistic_text)
+
+
+# подсчет информации о тексте
+def statistic_text(message):
+    stat = [0, 0, 0, 0, 0]
+    for i in message.text:
+        stat[0] += 1
+        if i.lower() in "aeiouy" or i.lower() in "ауоыэяюёие":
+            stat[1] += 1
+        if i.lower() in "bcdfghjklmnpqrstvwxz" or i.lower() in "бвгджзйклмнпрстфхцчшщ":
+            stat[2] += 1
+        if i in "1234567890":
+            stat[3] += 1
+        if i in ".?!,;:-":
+            stat[4] += 1
+    stat.append(len(message.text.split()))
+    if message_user.lower() == "количество слов":
+        return agent_ghost.send_message(message.chat.id, f"Количество слов: {stat[5]}")
+    elif message_user.lower() == "количество символов":
+        return agent_ghost.send_message(message.chat.id, f"Количество символов: {stat[0]}")
+    elif message_user.lower() == "количество гласных букв":
+        return agent_ghost.send_message(message.chat.id, f"Количество гласных букв: {stat[1]}")
+    elif message_user.lower() == "количество согласных букв":
+        return agent_ghost.send_message(message.chat.id, f"Количество согласных букв: {stat[2]}")
+    elif message_user.lower() == "количество гласных и согласных букв":
+        return agent_ghost.send_message(message.chat.id, f"Количество гласных букв: {stat[1]}\nКоличество согласных букв: {stat[2]}")
+    elif message_user.lower() == "количество цифр":
+        return agent_ghost.send_message(message.chat.id, f"Количество цифр: {stat[3]}")
+    elif message_user.lower() == "количество спец. символов":
+        return agent_ghost.send_message(message.chat.id, f"Количество спец. символов: {stat[4]}")
+    elif message_user.lower() == "всю информацию":
+        with open("stat.txt", "w") as file:
+            file.write(f"""Количество символов: {stat[0]}\nКоличество слов: {stat[5]}\nКоличество цифр: {stat[3]}\n
+            Количество знаков пунктуации: {stat[4]}\nКоличество гласных букв: {stat[1]}\nКоличество согласных букв: {stat[2]}""")
+        return agent_ghost.send_document(message.chat.id, open("stat.txt", "r"))
 
 
 # проверка на "счастливое" число
@@ -187,7 +365,7 @@ def par(message):
 
 
 # функция проверки на палиндром
-def palindrom(message):
+def palindrome(message):
     if re.findall(r"\d+", message.text):
         return agent_ghost.send_message(message.chat.id, "Без цифр пожалуйста")
     else:
--- Python/telegram_bot/dz1 palindrom/test.py
+++ Python/telegram_bot/dz1 palindrom/test.py
@@ -1,4 +1,10 @@
-a = ['1460.0']
-b = ""
-b.join(a)
-print(b)
+import json
+with open("reg.json", "r") as file:
+    all_info = json.load(file)
+for i in range(len(all_info.get('musicbot'))):
+    print("i = ", i)
+    print(all_info['musicbot'][i])
+    print(all_info['musicbot'][i]['login'])
+# all_info.get('musicbot').append(info)
+# with open("reg.json", "w") as file:
+#     json.dump(all_info, file)
